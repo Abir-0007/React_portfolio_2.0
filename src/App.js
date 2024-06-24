622f679f0d3e1ce1ccffa91bc6bd5c3fa9686ea3
Abir Kumar Nandi <abirnandi12345@gmail.com>
2024-06-24 23:23:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Social_links from "./components/Social_links";
+import Portfolio from "./components/Portfolio";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
+import ParticlesComponent from './components/particles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+       <Navbar />
+       <Home />
+       <Social_links />
+       <About />
+       <Portfolio />
+       <Experience />
+       <Contact />
+       <ParticlesComponent />
+      
+       
     </div>
+ 
   );
 }
 
